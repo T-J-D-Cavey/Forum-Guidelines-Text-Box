cc7ee2421227a8eb75ec8153768406b39216cac9
Tim Cavey <caveyt@hotmail.co.uk>
2023-04-05 11:00:41 +0100

    add script

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // Scripts:
 
+
+const htmlSidebarWidget = document.querySelector("body > main > div.full-width.Template-content > div > div.col.col--side.Sidebar.qa-div-sidebar.custom-sidebar > div > div:nth-child(1) > div");
+  
+if (!window.location.href.includes('topic/new')) {
+	htmlSidebarWidget.style.display = 'none';
+    }
